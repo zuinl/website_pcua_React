dcf41e9da1441736a96b07e62b56619d7eaf0187
Leonardo Zuin <leonardosoareszuin@gmail.com>
2020-06-03 18:51:41 -0300

    Adicionadas screenshots e botão de detalhes da turma

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,15 +6,17 @@ import './style.css'
 export default props => {
     return (
         <div className="footer">
-            <div class="row">
+            <div className="row">
                 <div className="col-12">
                     <p className="text footer-text">O Programa Comum-Unidade em Ação é de autoria 
                         e responsabilidade da Prefeitura Municipal de Taubaté, através da Secretaria de 
                         Esportes e Lazer. Detalhes sobre o Programa podem ser 
-                        encontrados <a href="" target="_blank">aqui</a>.</p>
+                        encontrados <a href="http://www.taubate.sp.gov.br/secretarias/seel/sistema/" 
+                        target="_blank" rel="noopener noreferrer">aqui</a>.</p>
                     <p className="text footer-text">Este <i>website</i> foi desenvolvido 
-                    por <a href="https://linkedin.com/in/lzuin" target="_blank">Leonardo Zuin</a> e o 
-                    código-fonte está disponível <a href="" target="_blank">aqui</a>.</p>
+                    por <a href="https://linkedin.com/in/lzuin" target="_blank" rel="noopener noreferrer">Leonardo Zuin</a> e o 
+                    código-fonte está disponível <a href="https://github.com/zuinl/website_pcua_React" 
+                    target="_blank" rel="noopener noreferrer">aqui</a>.</p>
                 </div>
             </div>
         </div>
--- src/components/Result/SportClass/SportClass.js
+++ src/components/Result/SportClass/SportClass.js
@@ -4,6 +4,7 @@ import '../../../App.css'
 import './style.css'
 
 export default props => {
+    const detailsLink = "http://www.taubate.sp.gov.br/secretarias/seel/sistema/verLista.php?id="
     return (
         <div className="class-container center">
             <div className="row">
@@ -13,6 +14,10 @@ export default props => {
                     <p className="text"><b>Projeto: </b>{props.project}</p>
                     <p className="text"><b>Local: </b>{props.address}</p>
                     <p className="text"><b>Instrutor: </b>{props.instructor}</p>
+
+                    <a href={detailsLink + props.id} target="_blank" rel="noopener noreferrer">
+                        <button className="details-button">Mais...</button>
+                    </a>
                 </div>
             </div>
         </div>
